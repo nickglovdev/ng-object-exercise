da25006a8a2b790f6f7eaa936d0c0238d73866ce
nickglovdev <nickglovdev@gmail.com>
2020-05-30 17:21:34 -0500

    Finished up the chapter with the Beatles excercise now onto the practice

--- scripts/main.js
+++ scripts/main.js
@@ -125,4 +125,40 @@ instructors in the console. */
 // const andy = nashvilleSoftwareSchool.instructors['fullTime'][4]
 // console.log(andy)
 
-console.log(`Hello`)
+const beatles = {
+    albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour", "Something New"],
+    history: {
+        formed: 1960,
+        disbanded: 1970
+    },
+    members: [
+        {
+            name: "George Harrison",
+            birth: 1943,
+            death: 2001
+        },
+        {
+            name: "Paul McCartney",
+            birth: 1942,
+            death: null
+        },
+        {
+            name: "John Lennon",
+            birth: 1940,
+            death: 1980
+        },
+        {
+            name: "Ringo Starr",
+            birth: 1940,
+            death: null
+        }
+    ]
+}
+
+
+console.log(`${beatles.members[1]["name"]} was in the Beatles from ${beatles.history["formed"]} to ${beatles.history["disbanded"]}. He was born in ${beatles.members[1]["birth"]}. He constributed heavily to the ${beatles.albums["3"]} Album.`)
+
+
+
+
+
