35e5634cbce2d21ccb5d3591e5207a8f2fe5514d
nickglovdev <nickglovdev@gmail.com>
2020-05-30 15:06:37 -0500

    Completed First group of questions.

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+/* Given two options of data structures - array or object - which should you use? 
+You need to represent a red 2015 Ford Mustang in your code. */
+
+// Answer: Object
+
+/* Given two options of data structures - array or object - which should you use? 
+You need to store list of animal names in a shelter. 
+The names are "Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", and "Sharky".*/
+
+//Answer: Array
+
+/*
+Define an object for four family members, and put each object in an array 
+named familyMembers. Each family member object that you create should have 
+the same keys on them, but the values will be different. */
+
+const familyMembers =
+    [
+        {
+            name: "John",
+            age:37,
+            title: "Father"
+        },
+        {
+            name: "Lorain",
+            age:34,
+            title: "Mother"
+        },
+        {
+            name: "Austin",
+            age:8,
+            title: "Son"
+        },
+        {
+            name: "Lilly",
+            age: 3,
+            title: "Daught"
+        }
+    ]
