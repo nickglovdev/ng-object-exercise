0250c26e0432f12c1e625c9b10c7c9c70b8118d3
nickglovdev <nickglovdev@gmail.com>
2020-05-30 15:56:33 -0500

    Answered more lighting questions all the way up to the empire state building.

--- scripts/main.js
+++ scripts/main.js
@@ -37,3 +37,61 @@ const familyMembers =
             title: "Daught"
         }
     ]
+
+
+/* Given the object below, output each of the values to the console using dot notation. */
+const wardrobe = {
+    height: 80,
+    manufacturer: "Killibrew & Sons",
+    contents: ["Dress shirt", "Jeans", "Suit", "Skirt", "Tennis shoes"],
+    depth: 38,
+    width: 50
+}
+
+// console.log(wardrobe.height)
+// console.log(wardrobe.manufacturer)
+// console.log(wardrobe.contents)
+// console.log(wardrobe.depth)
+// console.log(wardrobe.width)
+
+/* Add to the object */
+// wardrobe.material = "Cedar"
+// console.log(wardrobe.material)
+
+
+/* Time to practice dot and square-bracket notation. 
+Use the following object to complete the lightning exercises below. */
+
+const empireStateBuilding = {
+    stories: 103,
+    height: 1453,
+    address: "350 Fifth Avenue, Manhattan, New York 10118",
+    squareFeet: 2768591,
+    constructionDate: 1931,
+    cost: 40948900,
+    owner: "Empire State Realty Trust",
+    eastWestLength: 424,
+    northSouthLength: 187,
+    architect: "Shreve, Lamb & Harmon"
+}
+
+/* Use dot notation to output all of the dimensions of the 
+Empire State Building to the console. */
+
+console.log(empireStateBuilding.height)
+console.log(empireStateBuilding.eastWestLength)
+console.log(empireStateBuilding.northSouthLength)
+
+const stories = "stories"
+console.log(empireStateBuilding[stories])
+const address = "address"
+console.log(empireStateBuilding[address])
+const squareFeet = "squareFeet"
+console.log(empireStateBuilding[squareFeet])
+const cost = "cost"
+console.log(empireStateBuilding[cost])
+const owner = "owner"
+console.log(empireStateBuilding[owner])
+const architect = "architect"
+console.log(empireStateBuilding[architect])
+
